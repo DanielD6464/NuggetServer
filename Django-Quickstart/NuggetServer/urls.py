08d2a862497d33fe24a3ccc5beb7932429e5e6a5
daniel <dandouglass14@gmail.com>
2022-10-20 00:23:18 -0400

    login and register work

--- Django-Quickstart/NuggetServer/urls.py
+++ Django-Quickstart/NuggetServer/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from .views import myregister
 
 app_name = 'NuggetServer'
 urlpatterns = [
-    path('register/', myregister, name="registeruser"),
+    path('register/', views.register_request, name="register"),
     # path('home/', views.myhomepage, name='homepage'),
     path('flash/', views.mycreate, name='FlashWifiNugget'),
     path('database/', views.mycreate, name='MACDatabase'),
