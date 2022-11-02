883b243113a7a8eba9223b7806cbc6611b690ccd
daniel <dandouglass14@gmail.com>
2022-11-02 01:06:49 -0400

    starting on homepage

--- Database-Crud/NuggetServer/views.py
+++ Database-Crud/NuggetServer/views.py
@@ -41,21 +41,38 @@ def mac_database(request):
 		headers = {
 		}
 		response = requests.request("GET", url, headers=headers, data=payload)
-		print(response.text)
+		print("vendor:", response.text)
 		vendor = response.text
 		Vendor.objects.create(
 			mac_addr = mac,
 			ip_addr = ip,
 			vendor_name = vendor
 		)
+		# print("Vendor:", Vendor.objects.create())
+		return redirect("NuggetServer:MACDatabase")
+	else:
 		context = {
 			'vendors': Vendor.objects.all()
 		}
 		return render(request, "NuggetServer/database.html", context)
-	else:
-		return render(request, "NuggetServer/database.html")
 
+# def updateIP(request):
+# 	#grap ip/mac ID
+# 	# updateVendor = vendor
+# 	# updateMAC = mac_addr
+# 	#update the form to change IP or MAC
+# 	if request.method == "POST":
+# 		ip = request.POST.get("ip_addr")
 
+	
+	#def to update MAC ID
+	#def to update IP address
+	#grab obj by MAC or IP, use filter (object.filter )
+	#previous value = new value (obj.ip = ip)
+
+# def delete(request):
+	#grap ip/mac ID\
+	#query set- obj[1].delete()
 
 
 
