30af0077ce0353934a3265b408a358067c9704fe
daniel <dandouglass14@gmail.com>
2022-11-02 22:18:44 -0400

    add quickstart back

---
+++ Django-Quickstart/NuggetServer/api.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+
+resp = requests.get('https://api.macvendors.com/FC-A1-3E-2A-1C-3')
+data = resp.json()
+print(data)
+
+
+
+# <?php
+#   $mac_address = "FC:FB:FB:01:FA:21";
+
+#   $url = "https://api.macvendors.com/" . urlencode($mac_address);
+#   $ch = curl_init();
+#   curl_setopt($ch, CURLOPT_URL, $url);
+#   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+#   $response = curl_exec($ch);
+#   if($response) {
+#     echo "Vendor: $response";
+#   } else {
+#     echo "Not Found";
+#   }
+# ?>
